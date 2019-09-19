58c92843d60d1eebdf86e8344cfd12fab106c807
shenzhanwen <1719164322@qq.com>
2019-09-19 02:34:56 +0000

    模板布局

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,51 @@
+<head>
+	<meta charset="UTF-8">
+	<title>Mstore - Online Shop Mobile Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1  maximum-scale=1 user-scalable=no">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-touch-fullscreen" content="yes">
+	<meta name="HandheldFriendly" content="True">
+
+	<link rel="stylesheet" href="/in/css/materialize.css">
+	<link rel="stylesheet" href="/in/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/in/css/normalize.css">
+	<link rel="stylesheet" href="/in/css/owl.carousel.css">
+	<link rel="stylesheet" href="/in/css/owl.theme.css">
+	<link rel="stylesheet" href="/in/css/owl.transitions.css">
+	<link rel="stylesheet" href="/in/css/fakeLoader.css">
+	<link rel="stylesheet" href="/in/css/animate.css">
+	<link rel="stylesheet" href="/in/css/style.css">
+	
+	
+	<link rel="shortcut icon" href="/in/img/favicon.png">
+
+</head>
+<!-- navbar top -->
+	<div class="navbar-top">
+		<!-- site brand	 -->
+		<div class="site-brand">
+			<a href="index.html"><h1>Mstore</h1></a>
+		</div>
+		<!-- end site brand	 -->
+		<div class="side-nav-panel-right">
+			<a href="#" data-activates="slide-out-right" class="side-nav-left"><i class="fa fa-user"></i></a>
+		</div>
+	</div>
+	<!-- end navbar top -->
+
+	<!-- side nav right-->
+	<div class="side-nav-panel-right">
+		<ul id="slide-out-right" class="side-nav side-nav-panel collapsible">
+			<li class="profil">
+				<img src="/in/img/profile.jpg" alt="">
+				<h2>John Doe</h2>
+			</li>
+			<li><a href="setting.html"><i class="fa fa-cog"></i>Settings</a></li>
+			<li><a href="about-us.html"><i class="fa fa-user"></i>About Us</a></li>
+			<li><a href="contact.html"><i class="fa fa-envelope-o"></i>Contact Us</a></li>
+			<li><a href="login.html"><i class="fa fa-sign-in"></i>Login</a></li>
+			<li><a href="register.html"><i class="fa fa-user-plus"></i>Register</a></li>
+		</ul>
+	</div>
+	<!-- end side nav right-->
